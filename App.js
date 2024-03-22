f091ad3004fd6fd0a9eec75d5283002cd7470d4c
JoaquinAdan <joacorodriguez.98l@gmail.com>
2024-03-22 19:46:59 -0300

    Fix import statements and update background color

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import LoginForm from './src/components/LoginForm'
+// View o ScrollView: Contenedor de elementos
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola mundo!</Text>
-      <StatusBar style="auto" />
+      <LoginForm />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ed2224',
+    backgroundColor: '#fafafa',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
